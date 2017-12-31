3b6db5562951de0ca14e2cc17cc5c5222468d3d9
Evan Nordquist <nordquist.evan@gmail.com>
2017-12-31 04:12:36 -0500

    Update myScript_dev2.js
    - Added "Star" scoring system. 
    - Added a game timer.
    - Added a restart button to the 'you win' modal.
    - The modal summarizes the players: game time, number of moves, and their star rating for the round.

--- js/myScript_dev2.js
+++ js/myScript_dev2.js
@@ -41,6 +41,11 @@ var moves = 0;//How many moves (of two selections) have been made in the game.
 var matches = 0;//How many successful pairs were found.
 var scoreboardMoves = $('.movesTaken'); //To easily substitute in the current moves taken.
 var modal = $('#myModal');//Selecting the Modal element (the "congrats message" at the end).
+var isTimerRunning = false;
+var timerExperiment; //to take a snapshot of the timer when the game ends.
+var starKeeper; // Controls when the stars dissapear from the scoreboard.
+var starNumKeeper = 3; //Keeps a running count of the stars.
+var span = $('.close'); //The 'X' button to close the modal.
 
 
 //****************//
@@ -62,7 +67,7 @@ function shuffle(array) {
 }
 
 function nextRound() {
-	//Upkeep phase, turning cards back over, resetting numCardsOpen to zero
+	//Upkeep phase, turning cards back over, resetting numCardsOpen to zero.
 	numCardsOpen = 0;
 	$('.open').removeClass('open');
 	$('.show').removeClass("show");
@@ -70,21 +75,39 @@ function nextRound() {
 }
 
 function scoreboardUpdate() {
-	//Update the scoreboard with the most recent move totals
+	//Update the scoreboard with the most recent move totals, and periodically remove "stars".
 	moves = moves + 1;
 	scoreboardMoves.text(moves);
+	//At intervals of 16, 20, and 25 moves, a star is removed from the player's scoreboard.
+	if (moves === 16){
+		starKeeper = $('#star1');
+		starKeeper.remove();
+		starNumKeeper = starNumKeeper -1;
+	} else if (moves === 20){
+		starKeeper = $('#star2');
+		starKeeper.remove();
+		starNumKeeper = starNumKeeper -1;
+	} else if (moves === 25){
+		starKeeper = $('#star3');
+		starKeeper.remove();
+		starNumKeeper = starNumKeeper -1;
+	} else {
+		//carry on
+	}
 }
 
 function updateCongratsMessage() {
 	//When the game is over, update the #gratsText element with a customized message
-	var tempMessage = $('#gratsText');
-	tempMessage.text("Congrats! You won! Your only took " + moves +" moves!");
+	var tempMessage1 = $('#gratsText1');
+	var tempMessage2 = $('#gratsText2');
+	tempMessage1.text("Your only took " + moves +" moves and " + timerExperiment + " seconds to complete it!");
+	tempMessage2.text("Your star rating for this round is " + starNumKeeper + '!');
 }
 
 function checkGameStatus() {
 	//Whenever a match is found, check to see if you are at 8 matches (all complete).
 	//If matches === 8, unhide the modal.
-	//if not, keep playing as normal.
+	//If not, keep playing as normal.
 	if (matches === 8){
 		updateCongratsMessage()
 		modal.css("display", "block");
@@ -93,9 +116,8 @@ function checkGameStatus() {
 	}
 }
 
-
 function checkPairStatus() {
-	//After you have two cards selected check to see if they are a pair
+	//After you have two cards selected, check to see if they are a pair.
 	//First, make sure they didn't just pick the same exact card twice (id's have to be different)
 	//If you have a match, increment the matches variable, update the scoreboard, check to see if the game is over.
 	if (cardChoice1ID != cardChoice2ID){
@@ -103,33 +125,62 @@ function checkPairStatus() {
 		console.log(cardChoice1);
 		console.log(cardChoice2);
 			if (cardChoice1 === cardChoice2) {
-			console.log("ITS A MATCH!");
-			scoreboardUpdate();
-			$('.open').addClass('match');
-			matches = matches + 1;
-			console.log("matches: " +matches);
-			nextRound();
-			checkGameStatus();
-			}
-		else {
+				console.log("ITS A MATCH!");
+				scoreboardUpdate();
+				$('.open').addClass('match');
+				matches = matches + 1;
+				console.log("matches: " +matches);
+				nextRound();
+				checkGameStatus();
+			} else {
 			//If the two cards are not a match, update the scoreboard.
 			//Give the user a brief chance to see the mismatched pair.
 			//reset the cards with the nextRound() function.
 			console.log("It's not a match!");
 			scoreboardUpdate();
 			setTimeout(function(){
-				nextRound();},700);
-			};
-		}
-	else {
+				nextRound();
+			},700);
+			}
+	} else {
 		//If the user clicked the same card twice, it is not a pair, even though both 'choices' have the same class name.
 		//If the second card has the same ID, it is not cointed, the number of selections remains at 1.
 		//The game continues as if you had not yet selected your second card. 
 		console.log("You can't pick the same card!");
 		numCardsOpen = 1;
+		};
+}
+
+function startTheTimer() {
+	//If the timer is not running, start it.
+	//If the timer is running leave it be.
+	if (isTimerRunning === false){
+		isTimerRunning = true;
+		timerMechanism();
+	} else {
+		//All is well.
 	};
 }
 
+
+function timerMechanism() {
+	//timerMechanism code as per Udacity Project Reviewers suggested method
+	const game_start_time = new Date().getTime(); // get the current time when user clicked the first card
+	timer = setInterval(function(){
+		let current_time = new Date().getTime();
+		let current_time_played = current_time - game_start_time;
+		let hrs = Math.floor((current_time_played % (1000 * 60 * 60 * 24))/ (1000 * 60 * 60));
+		let mins = Math.floor((current_time_played % (1000 * 60 * 60))/ (1000 * 60));
+		let secs = Math.floor((current_time_played % (1000 * 60))/ 1000);
+		time_value = hrs + ' hours ' + mins + ' mins ' + secs + ' secs ';
+		if (secs <10) {
+			secs = '0' + secs;
+		}
+		current_time_played = hrs + ':' + mins + ':' + secs;
+		$(".time-played").text(current_time_played);
+		timerExperiment = current_time_played;
+	}, 500);
+};
 
 //****************//
 //Main Game Logic*//
@@ -146,6 +197,9 @@ for (var i = cards.length - 1; i >= 0; i--) {
 
 // set up the event listener for a card. 
 $('.deck').on('click', '.card', function(evt){
+	startTheTimer(); //Start the timer, the first time a card is clicked.
+	var starsCompilation = $('.stars').find('li');
+	console.log(starsCompilation);
 	if (numCardsOpen >=2){
 		console.log("You can't select more than 2 cards");
 	}
@@ -177,27 +231,19 @@ $('.deck').on('click', '.card', function(evt){
 			checkPairStatus();
 		}
 		else {
-			console.log("test to make sure you can't have more than2 cards open.");
+			console.log("test to make sure you can't have more than 2 cards open.");
 		}
 	}
 });
 
-$(".restart").on('click', function(){
-	location.reload();
-});
-
-//var modal = $('#myModal');
-//var btn = $('#myBtn')
-var span = $('.close');
-/*btn.on('click', function() {
-	console.log("button was pressed");
-	location.reload();
-	//modal.css("display", "block");
-});
-*/
-
-//Closing (or rather hiding, the modal when you click the 'X' button)
-span.on('click', function() {
+$(".close").on('click', function() {
+	//Closing (or rather hiding) the modal when you click the 'X' button)
 	console.log("modal close button was pressed");
 	modal.css("display", "none");
+});
+
+$(".restart").on('click', function(){
+	//Clicking anything with class 'restart' (above the game board, or inside the modal)
+	//Causes the page to reload (resetting all variables and timers).
+	location.reload();
 });
